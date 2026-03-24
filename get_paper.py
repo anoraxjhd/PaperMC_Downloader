bc9e9897469413259e284f6742f60e96e3b36c34
anoraxjhd <122669717+anoraxjhd@users.noreply.github.com>
2026-03-24 15:01:44 +0100

    Implement translation feature and enhance error messages
    Added translation support and improved error handling.

--- get_paper.py
+++ get_paper.py
@@ -2,36 +2,55 @@
 from requests import get
 import customtkinter as ctk
 from tkinter import messagebox as mb
-from webbrowser import open
+from webbrowser import open as wbopen
+from json import load
+
+
+# VARS
+paperURL = ""
 
 # Settings
+
+customLang = True
+lang = "de"
 ctk.set_appearance_mode("dark")
 URL = "https://qing762.is-a.dev/api/papermc/"
-allowedStatus = [
-  200, 203
-]
+allowedStatus = [200, 203]
+if customLang:
+  with open("translations.json", "r", encoding="UTF-8") as f:
+    translations = load(f)
+
+def translate(text, lang="en"):
+  try:
+    return translations.get(lang, {}).get(text, text)
+  except:
+    return text
 
 def getData():
-  global data
+  global data, status
   response = get(URL)
   status = response.status_code
   if status in allowedStatus:
     data = response.json()
   else:
-    mb.showerror(f"Fehler {status}", f'Servers response is: {status}')
+    mb.showerror(str(translate("Error", lang=lang)) + " " + str(status), f'{translate("Servers response is", lang=lang)}: {status}')
 
 def send(version = "", data = ""):
   global paperURL
   if version in data['versions']:
-    resultLabel.configure(text=f"Paper {version} Gefunden. \n Drücke Auf ""Download"" um Paper herunterzuladen.")
+    resultLabel.configure(text=f"Paper {version} {translate("found", lang=lang)}. \n {translate("Press ""Download"" to download Paper", lang=lang)}.")
     paperURL = data['versions'][version]
   else:
-    resultLabel.configure(text=f"Paper {version} nicht gefunden.")
+    resultLabel.configure(text=f"Paper {version} {translate("not found", lang=lang)}.")
 
 def beforeSend(version = "", data = ""):
-  if not data: mb.showerror(f"Fehler", f'Data is None are you Online?'); return 1
-  if not version: resultLabel.configure(text=f"Gebe eine Version ein."); return 1
+  if not data: mb.showerror(translate("Error", lang=lang), f'{translate("Data is None are you Online", lang=lang)}?'); return 1
+  if not version: resultLabel.configure(text=f"{translate("Enter a version", lang=lang)}."); return 1
   send(version, data)
+
+def openBrowser(URL):
+  if not URL: resultLabel.configure(text=f"{translate("Enter a Version and Click Send", lang=lang)}."); return 1
+  wbopen(URL)
 
 def GUI():
   global resultLabel, versionInput
@@ -43,16 +62,16 @@ def GUI():
   resultLabel = ctk.CTkLabel(app, text="/")
   resultLabel.pack(padx=20, pady=10)
 
-  download = ctk.CTkButton(app, text="Herunterladen", command=lambda: open(paperURL))
+  download = ctk.CTkButton(app, text=translate("Download", lang=lang), command=lambda: openBrowser(paperURL))
   download.pack(padx=50)
 
-  versionInput = ctk.CTkEntry(app, placeholder_text="Version")
+  versionInput = ctk.CTkEntry(app, placeholder_text=translate("Version", lang=lang))
   versionInput.pack(padx=20, pady=20)
 
-  submit = ctk.CTkButton(app, text="Senden", command=lambda: beforeSend(version=versionInput.get(), data=data))
+  submit = ctk.CTkButton(app, text=translate("Send", lang=lang), command=lambda: beforeSend(version=versionInput.get(), data=data))
   submit.pack(padx=20)
 
   app.mainloop()
 
 getData()
-GUI()
+if status in allowedStatus: GUI()
