c479add3d395793dc69aba5badee29ee62d3bfde
Anoraxjhd <felix.schenk1611@gmail.com>
2026-04-25 11:31:08 +0200

    - Now using Argparse lib for argument parsing

--- main.py
+++ main.py
@@ -1,29 +1,19 @@
 # Imports
-from sys import argv
 import src.ui as ui
 import src.vars as vars
 from os import path
+from argparse import ArgumentParser
 
 vars.savePath = path.dirname(path.abspath(__file__))
 
 def parseArgs():
-  global lang, no_gui
-  i = 1
-  while i < len(argv):
-    arg = argv[i]
-    if arg == '--lang':
-      if i + 1 < len(argv) and argv[i + 1] in vars.supportedLanguages:
-        vars.lang = argv[i + 1]
-        i += 1
-      else:
-        print("Unsupported language or missing value after --lang.")
-        exit(1)
-    elif arg == '--no-gui':
-      vars.no_gui = True
-    else:
-      print(f"Unknown argument: {arg}")
-      exit(1)
-    i += 1
+  parser = ArgumentParser()
+  parser.add_argument("--lang", choices=vars.supportedLanguages, default=vars.lang)
+  parser.add_argument("--no-gui", action="store_true")
+
+  args = parser.parse_args()
+  vars.lang = args.lang
+  vars.no_gui = args.no_gui
 
 parseArgs()
 if vars.no_gui:
